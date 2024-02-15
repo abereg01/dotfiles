90b3d2efbd636106e02c7182bd7b508d1780b178
abereg01 <abereg01@pm.me>
2024-02-15 10:59:36 +0100

    save

--- .config/fish/functions/gc.fish
+++ .config/fish/functions/gc.fish
@@ -1,4 +1,4 @@
-function gc --wraps='git clone' --description 'alias gc=git clone'
-  git clone $argv
+function gc --wraps='git clone' --wraps=git\ commit\ -m\ \'save\' --description alias\ gc=git\ commit\ -m\ \'save\'
+  git commit -m 'save' $argv
         
 end
--- .config/fish/functions/gsave.fish
+++
@@ -1,4 +0,0 @@
-function gsave --wraps=git\ commit\ -m\ \'save\' --description alias\ gsave=git\ commit\ -m\ \'save\'
-  git commit -m 'save' $argv
-        
-end
