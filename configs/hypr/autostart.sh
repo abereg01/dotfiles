34512f80812e0a8a5f396e06f719118cda7ec2b0
keabandreasdesktop <andreas.bergoltz@karlshamnenergi.se>
2025-08-07 11:26:53 +0200

    fedora

--- configs/hypr/autostart.sh
+++ configs/hypr/autostart.sh
@@ -7,8 +7,6 @@ hyprctl setcursor left_ptr 24
 exec-once: swww init
 exec: swww img /home/abe/lib/images/wallpapers/_sort/ev-U-8pVp66LOQ-unsplash.jpg &
 
-split-monitor-workspaces &
-
 # Services
 swww-daemon &
 dunst &
