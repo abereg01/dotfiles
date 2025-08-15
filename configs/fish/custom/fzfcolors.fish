7427ef57f13532511384f036aac8629c35eb731f
keabandreasdesktop <andreas.bergoltz@karlshamnenergi.se>
2025-08-15 08:44:32 +0200

    hyr

--- configs/fish/custom/fzfcolors.fish
+++ configs/fish/custom/fzfcolors.fish
@@ -1,27 +1,10 @@
-# Color configuration
-set -g color_background #2E3440
-set -g color_foreground #D8DEE9
-set -g color_black #3B4252
-set -g color_red #BF616A
-set -g color_green #A3BE8C
-set -g color_yellow #EBCB8B
-set -g color_blue #81A1C1
-set -g color_magenta #B48EAD
-set -g color_cyan #88C0D0
-set -g color_white #E5E9F0
-
-# Additional color mappings for fzf
-set -g color_fg $color_foreground
-set -g color_bg $color_background
-set -g color_hl $color_yellow
-set -g color_fg_plus $color_white
-set -g color_bg_plus $color_black
-set -g color_hl_plus $color_yellow
-set -g color_border $color_black
-set -g color_header $color_blue
-set -g color_gutter $color_background
-set -g color_spinner $color_yellow
-set -g color_info $color_cyan
-set -g color_pointer $color_magenta
-set -g color_marker $color_red
-set -g color_prompt $color_foreground
+set -g color_background #2f383e
+set -g color_foreground #d3c6aa
+set -g color_black #374247
+set -g color_red #e67e80
+set -g color_green #a7c080
+set -g color_yellow #dbbc7f
+set -g color_blue #7fbbb3
+set -g color_magenta #d699b6
+set -g color_cyan #83c092
+set -g color_white #859289
--- configs/hypr/autostart.sh
+++ configs/hypr/autostart.sh
@@ -5,7 +5,7 @@ hyprctl setcursor left_ptr 24
 
 # Wallpaper (swww)
 exec-once: swww init
-exec: swww img /home/abe/lib/images/wallpapers/_sort/ev-U-8pVp66LOQ-unsplash.jpg &
+#exec: swww img /home/abe/lib/images/wallpapers/_sort/ev-U-8pVp66LOQ-unsplash.jpg &
 
 # Services
 swww-daemon &
